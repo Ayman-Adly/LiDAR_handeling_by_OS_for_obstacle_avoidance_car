de1be1d8323822616ee87462bf94b32a0edea5c9
Ehab <ehab.ashraf99@gmail.com>
2023-02-16 12:31:52 +0200

    using the "TaskInfo" abstracted from the handling of the linkedlist "Task_t" is linked list node implementation

--- OS/RTOS_Priv.h
+++ OS/RTOS_Priv.h
@@ -14,14 +14,19 @@ TASK_READY
 
 typedef struct
 {
-
 	u8 Priority		   ;
 	u16 periodicity        ;
 	void (*TaskFunc)(void) ;
 	TaskState_t TaskState  ;
 	u16 FirstDelay	   	   ;
+}TaskInfo_t;
+
+typedef struct
+{
+	TaskInfo_t Task		   ;
 	struct Task_t *Next	   ;	
 }Task_t;
+
 
 
 static void vidSchedular(void);
